6a5b9732877d3c0037c51bcfe0b2beb6be8d702d
Generation Hope <generationhope@Generations-MacBook-Air.local>
2019-09-09 16:38:22 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/Classwork.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import java.util.Scanner;
+
+public class Classwork {
+    public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
+
+        String name= keyboard.nextLine 
+    }
+
+System.out.println("Name :"+name );
+System.out.println("GPA :"+gpa);
+System.out.println("Credits :"+credits);
+
+if (gpa >= 1.0 && credits >= 120 && acct==0.0 && major_gpa >= 2.5
+
+    {
+        System.out.print("Can Graduate");
+    }else
+    {
+        System.out.print("Can Not Graduate");
+  }
+  System.out.print(name.charAt(1));
+}
