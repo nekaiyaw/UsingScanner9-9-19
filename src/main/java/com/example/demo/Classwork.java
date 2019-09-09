f3e3cd38f448aecf889bb1e808e02518f1ea0d8a
Nekaiya Wilson <nekaiya.wilson@bison.howard.edu>
2019-09-09 17:18:22 -0400

    Second Attempt

--- src/main/java/com/example/demo/Classwork.java
+++ src/main/java/com/example/demo/Classwork.java
@@ -7,20 +7,26 @@ public class Classwork {
 
         Scanner keyboard = new Scanner(System.in);
 
-        String name= keyboard.nextLine 
+        String name = keyboard.nextLine();
+        double gpa = keyboard.nextDouble();
+        int credits  = keyboard.nextInt();
+        double major_gpa = keyboard.nextDouble();
+        double acct = keyboard.nextDouble();
+
+        System.out.println("Name :" + name );
+        System.out.println("GPA :" + gpa );
+        System.out.println("Credits :" + credits);
+
+        if (gpa >= 1.0 && credits >= 120 && acct==0.0 && major_gpa >= 2.5)
+        {
+            System.out.print("Can Graduate");
+        } else
+        {
+            System.out.print("Can NOT Graduate");
+        }
+
+        System.out.print(name.charAt(1));
+
     }
 
-System.out.println("Name :"+name );
-System.out.println("GPA :"+gpa);
-System.out.println("Credits :"+credits);
-
-if (gpa >= 1.0 && credits >= 120 && acct==0.0 && major_gpa >= 2.5
-
-    {
-        System.out.print("Can Graduate");
-    }else
-    {
-        System.out.print("Can Not Graduate");
-  }
-  System.out.print(name.charAt(1));
 }
